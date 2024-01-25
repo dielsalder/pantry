ad6b171a963c65ac1deb03a5a1d244d2bc784b96
Jude He <>
2024-01-24 20:15:46 -0500

    Create collections

--- pantry/src/features/Home.tsx
+++ pantry/src/features/Home.tsx
@@ -1,11 +1,34 @@
-import { AppShell, Button, Group, Loader, Title } from "@mantine/core";
+import {
+  AppShell,
+  Button,
+  Group,
+  Loader,
+  Modal,
+  TextInput,
+  Title,
+} from "@mantine/core";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { api } from "~/utils/api";
 import { Collection } from "./Collection";
+import { useQueryClient } from "@tanstack/react-query";
+import { useDisclosure } from "@mantine/hooks";
+import { useForm } from "@mantine/form";
+import { getQueryKey } from "@trpc/react-query";
 
 export const Home = () => {
   const { data: sessionData } = useSession();
   const { data, isLoading } = api.user.read.useQuery();
+  const queryClient = useQueryClient();
+  const [opened, { open, close }] = useDisclosure();
+  const form = useForm({
+    initialValues: { name: "" },
+    validate: {
+      name: (value) => (value.length < 1 ? "Please enter name" : null),
+    },
+  });
+  const { mutate } = api.collection.create.useMutation({
+    onSuccess: () => queryClient.invalidateQueries(getQueryKey(api.user.read)),
+  });
   return (
     <>
       <AppShell.Header p="md">
@@ -24,6 +47,22 @@ export const Home = () => {
         ) : (
           data?.collections.map(({ id }) => <Collection id={id} key={id} />)
         )}
+        <Button onClick={open}>New Collection</Button>
+        <Modal opened={opened} onClose={close} title="New Collection">
+          <form
+            onSubmit={form.onSubmit((data) => {
+              mutate(data.name);
+              close();
+            })}
+          >
+            <TextInput label="Name" {...form.getInputProps("name")} />
+            <Group justify="flex-end" mt="md">
+              <Button variant="filled" color="blue" type="submit">
+                Save
+              </Button>
+            </Group>
+          </form>
+        </Modal>
       </AppShell.Main>
     </>
   );
